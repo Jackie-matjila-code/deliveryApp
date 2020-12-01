b8875e0470c37f4df87ef96af7dccff54b103045
matjilatj2@gmail.com <matjilatj2@gmail.com>
2020-12-01 23:51:56 +0200

    added sign in/login

--- src/app/Components/sig-in/sig-in.page.ts
+++ src/app/Components/sig-in/sig-in.page.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import firebase from 'firebase/app';
 @Component({
   selector: 'app-sig-in',
   templateUrl: './sig-in.page.html',
   styleUrls: ['./sig-in.page.scss'],
 })
 export class SigInPage implements OnInit {
-
-  constructor() { }
+  email:string = "";
+  password:string = "";
+  constructor(public afAuth: AngularFireAuth, public router: Router) { }
 
   ngOnInit() {
   }
-
+async login(){
+  const {email, password} = this
+  try{
+    const res = await this.afAuth.signInWithEmailAndPassword(email, password);
+    if(res){
+        this.router.navigateByUrl('tabs/home');
+    }
+  }catch(error){
+    console.dir(error());
+    if(error.code === "user-not-found"){
+      console.log("User not found");
+    }
+    
+  }
+}
 }
--- src/app/Components/sign-up/sign-up.page.ts
+++ src/app/Components/sign-up/sign-up.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
+email:string = "";
+password:string = "";
+cpassword:string = "";
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth, public router: Router) { }
 
   ngOnInit() {
   }
-
+  async register(){
+    const {email, password, cpassword} = this
+    if(password !== cpassword){
+      return console.error("Passwords do not match");
+    }try{
+      const res = await this.auth.createUserWithEmailAndPassword(email, password);
+      console.log(res);
+      console.log("User created");
+      if(res){
+        this.router.navigateByUrl('sig-in');
+    }
+      
+    }catch(error){
+      console.dir(error);
+      if(error.code === "auth/email-already-in-use"){
+         this.router.navigateByUrl('sig-in');
+      }
+    }
+    
+  }
 }
